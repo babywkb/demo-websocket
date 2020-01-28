bf3be259db489c187609293d874cbf79078a9d36
takatani.hiroki <takatani.hiroki@tis.co.jp>
2020-01-28 12:08:37 +0900

    ユーザー切り分け

--- front/src/components/Chat.js
+++ front/src/components/Chat.js
@@ -2,10 +2,15 @@ import React from "react";
 import SockJsClient from "react-stomp";
 import Fetch from "json-fetch";
 import { TalkBox } from "react-talk";
+import UsernameGenerator from "username-generator"
+
+const randomstring = require("randomstring");
 
 class Chat extends React.Component {
   constructor(props) {
     super(props);
+    this.randomUserName = UsernameGenerator.generateUsername("-");
+    this.randomUserId = randomstring.generate();
     this.state = {
       clientConnected: false,
       messages: []
@@ -31,25 +36,40 @@ class Chat extends React.Component {
   componentWillMount() {
     Fetch("/history", {
       method: "GET"
-    }).then((response) => {
+    }).then(response => {
       this.setState({ messages: response.body });
     });
   }
 
   render() {
-    const wsSourceUrl = window.location.protocol + "//" + window.location.host + "/chat";
-    // const wsSourceUrl = 'http://localhost:8080/chat';
+    // const wsSourceUrl = window.location.protocol + "//" + window.location.host + "/handler";
+    const wsSourceUrl = "/handler";
     return (
       <div>
-        <TalkBox topic="react-websocket-template" currentUserId={ this.randomUserId }
-          currentUser={ this.randomUserName } messages={ this.state.messages }
-          onSendMessage={ this.sendMessage } connected={ this.state.clientConnected }/>
+        <TalkBox
+          topic="react-websocket-template"
+          currentUserId={this.randomUserId}
+          currentUser={this.randomUserName}
+          messages={this.state.messages}
+          onSendMessage={this.sendMessage}
+          connected={this.state.clientConnected}
+        />
 
-        <SockJsClient url={ wsSourceUrl } topics={["/topic/chat"]}
-          onMessage={ this.onMessageReceive } ref={ (client) => { this.clientRef = client }}
-          onConnect={ () => { this.setState({ clientConnected: true }) } }
-          onDisconnect={ () => { this.setState({ clientConnected: false }) } }
-          debug={ false }/>
+        <SockJsClient
+          url={wsSourceUrl}
+          topics={["/topic/all"]}
+          onMessage={this.onMessageReceive}
+          ref={client => {
+            this.clientRef = client;
+          }}
+          onConnect={() => {
+            this.setState({ clientConnected: true });
+          }}
+          onDisconnect={() => {
+            this.setState({ clientConnected: false });
+          }}
+          debug={true}
+        />
       </div>
     );
   }
